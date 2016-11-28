61b042b254378df5ae3f87ac48a8ab22d2fdddf2
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2016-11-28 12:11:37 +0000

    Já há postosgit add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql git add 3entrega/proj.sql !

--- 3entrega/proj.sql
+++ 3entrega/proj.sql
@@ -125,15 +125,28 @@ insert into Edificio values('Avenida Rovisco Pais');
 insert into Edificio values('Avenida Fontes Pereira de Melo');
 
 insert into Alugavel values('Avenida da Republica', '1', 'adsasdsa');
+insert into Alugavel values('Avenida da Republica', '11', 'adsasd');
+insert into Alugavel values('Avenida da Republica', '12', 'adsasds');
 insert into Alugavel values('Avenida da Republica', '2', 'vacasasac');
+insert into Alugavel values('Avenida da Republica', '21', 'vacasas');
+insert into Alugavel values('Avenida da Republica', '22', 'vacasasa');
 insert into Alugavel values('Avenida Rovisco Pais', '3', 'ascasca');
+insert into Alugavel values('Avenida Rovisco Pais', '31', 'ascas');
+insert into Alugavel values('Avenida Rovisco Pais', '32', 'ascasc');
 insert into Alugavel values('Avenida Rovisco Pais', '4', 'wqofnqofq');
+insert into Alugavel values('Avenida Rovisco Pais', '41', 'wqofnqo');
+insert into Alugavel values('Avenida Rovisco Pais', '42', 'wqofnqof');
 insert into Alugavel values('Avenida Fontes Pereira de Melo', '5', 'acqwwqw');
+insert into Alugavel values('Avenida Fontes Pereira de Melo', '51', 'acqww');
+insert into Alugavel values('Avenida Fontes Pereira de Melo', '52', 'acqwwq');
 insert into Alugavel values('Avenida Fontes Pereira de Melo', '6', 'acqwsavaw');
+insert into Alugavel values('Avenida Fontes Pereira de Melo', '61', 'acqwsav');
+insert into Alugavel values('Avenida Fontes Pereira de Melo', '62', 'acqwsava');
 
 insert into Arrenda values('Avenida da Republica', '1', 123456789);
 insert into Arrenda values('Avenida da Republica', '2', 123456788);
 insert into Arrenda values('Avenida Rovisco Pais', '3', 123456787);
+
 
 insert into Espaco values('Avenida da Republica', '1');
 insert into Espaco values('Avenida da Republica', '2');
@@ -141,6 +154,20 @@ insert into Espaco values('Avenida Rovisco Pais', '3');
 insert into Espaco values('Avenida Rovisco Pais', '4');
 insert into Espaco values('Avenida Fontes Pereira de Melo', '5');
 insert into Espaco values('Avenida Fontes Pereira de Melo', '6');
+
+insert into Posto values('Avenida da Republica', '11', '1');
+insert into Posto values('Avenida da Republica', '12', '1');
+insert into Posto values('Avenida da Republica', '21', '2');
+insert into Posto values('Avenida da Republica', '22', '2');
+insert into Posto values('Avenida Rovisco Pais', '31', '3');
+insert into Posto values('Avenida Rovisco Pais', '32', '3');
+insert into Posto values('Avenida Rovisco Pais', '41', '4');
+insert into Posto values('Avenida Rovisco Pais', '42', '4');
+insert into Posto values('Avenida Fontes Pereira de Melo', '51', '5');
+insert into Posto values('Avenida Fontes Pereira de Melo', '52', '5');
+insert into Posto values('Avenida Fontes Pereira de Melo', '61', '6');
+insert into Posto values('Avenida Fontes Pereira de Melo', '62', '6');
+
 
 insert into Reserva values('1');
 insert into Reserva values('2');
