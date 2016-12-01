507945c611425d09dff7f245e7a27636124db20f
DCalhas <davidcalhas@hotmail.com>
2016-12-01 17:39:07 +0000

    triggers feito

--- 3entrega/triggers.sql
+++ 3entrega/triggers.sql
@@ -3,8 +3,31 @@ create trigger overlaped_dates
 before insert on oferta
 for each row 
 begin
-	if exists(select * from oferta where ((new.data_inicio < oferta.data_fim OR new.data_fim < oferta.data_fim) AND new.codigo = oferta.codigo)
-	THEN signal SQLSTATE '45000'; /*BUT WHYYY*/
+	if exists(select * from oferta where ((new.data_inicio < oferta.data_fim OR new.data_fim < oferta.data_fim) AND new.codigo = oferta.codigo))
+	then call tuamae(); 
+	end if;
+end //
+delimiter ;
+
+
+delimiter //
+create trigger reserve_payment
+before insert on estado
+for each row 
+begin
+	if exists(select * from estado where numero = new.numero group by numero having new.time_stamp < max(time_stamp)) 
+	then call tuamae();
+	end if;
+end //
+delimiter ;
+
+delimiter //
+create trigger insert_payment
+before insert on paga
+for each row 
+begin
+	if exists(select * from estado where new.numero = numero and new.data = time_stamp) 
+	then call tuamae();
 	end if;
 end //
 delimiter ;
