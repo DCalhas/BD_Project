8c367dd9a8d4e1bec46fa3658037a23bea34d23f
DCalhas <davidcalhas@hotmail.com>
2016-12-02 15:23:08 +0000

    bla file removed

--- 3entrega/bla.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-	<?php
-		echo ("<form action=\"update.php\" method=\"post\">\n");
-		echo("<p>Nova Morada: <input type=\"text\" name=\"morada\"/></p>\n");
-		echo("<p><input type=\"submit\" value=\"Submit\"/></p>\n");
-		?>
-	</body>
-</html>
