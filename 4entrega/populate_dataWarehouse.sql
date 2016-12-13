c82987ffc6661fbbb1ddfbebedf3bbae7e0595d7
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2016-12-13 16:20:40 +0000

    populate dataWarehouse incompleto

--- 4entrega/populate_dataWarehouse.sql
+++ 4entrega/populate_dataWarehouse.sql
@@ -1,7 +1,36 @@
-insert into reserva_factos (duracao)
-select timestampdiff(day, data_inicio, data_fim) as duracao_reserva
-from oferta natural join aluga natural join reserva;
+insert into local_dimensao (local_id, posto_codigo, espaco_codigo, edificio_morada)
+select concat(morada, codigo) as local_id,
+	null as posto_codigo,
+	codigo as espaco_codigo,
+	morada as edificio_morada,
+from espaço;
 
-insert into reserva_factos (montante_pago)
-select tarifa * timestampdiff(day, data_inicio, data_fim) as montante_pago
-from oferta natural join aluga natural join reserva;
+
+insert into local_dimensao (local_id, posto_codigo, espaco_codigo, edificio_morada)
+select concat(morada, codigo_espaco, codigo) as local_id,
+	codigo as posto_codigo,
+	codigo_espaco as espaco_codigo,
+	morada as edificio_morada,
+from posto;
+
+insert into user_dimensao (nif, nome, telefone)
+select nif, nome, telefone
+from user;
+
+INSERT INTO data_dimensao
+SELECT @date := date_add(@date, interval 1 day) as date,
+    date_format(@date, "%Y%m%d") as id,
+    year(@date) as ano,
+    month(@date) as mes_do_ano,
+    day(@date) as dia_do_ano,
+    week(@date) as semana_do_ano,
+FROM T
+WHERE date_add(@date, interval 1 day) <= @d1
+ORDER BY date;
+
+
+insert into reserva_factos (numero, montante_pago, duracao)
+select numero, 
+	tarifa * timestampdiff(day, data_inicio, data_fim) as montante_pago, 
+	timestampdiff(day, data_inicio, data_fim) as duracao_reserva,
+from oferta natural join aluga;
--- 4entrega/starSchema.sql
+++ 4entrega/starSchema.sql
@@ -5,20 +5,6 @@ drop table if exists user_dimensao;
 drop table if exists tempo_dimensao;
 
 
-create table reserva_factos (
-    numero varchar(255) not null unique,
-    user_id varchar(9) not null,
-    local_id varchar(9) not null,
-    tempo_id varchar(9) not null,
-    data_id varchar(9) not null,
-    motante_pago numeric(19,4) not null,
-    duracao date not null,
-    foreign key(user_id) references user_dimensao(nif),
-    foreign key(local_id) references local_dimensao(local_id),
-    foreign key(tempo_id) references tempo_dimensao(tempo_id),
-    foreign key(data_id) references data_dimensao(data_id),
-    primary key(numero));
-
 create table user_dimensao (
     nif varchar(9) not null unique,
     nome varchar(80) not null,
@@ -26,7 +12,7 @@ create table user_dimensao (
     primary key(nif));
 
 create table local_dimensao (
-    local_id varchar(9) not null unique,
+    local_id varchar(765) not null unique,
     posto_codigo varchar(255) not null,
     espaco_codigo varchar(255) not null,
     edificio_morada varchar(255) not null,
@@ -47,3 +33,18 @@ create table tempo_dimensao (
     minuto_do_dia date not null,
     hora_do_dia date not null,
     primary key(tempo_id));
+
+
+create table reserva_factos (
+    numero varchar(255) not null unique,
+    user_id varchar(9) not null,
+    local_id varchar(9) not null,
+    tempo_id varchar(9) not null,
+    data_id varchar(9) not null,
+    montante_pago numeric(19,4) not null,
+    duracao date not null,
+    foreign key(user_id) references user_dimensao(nif),
+    foreign key(local_id) references local_dimensao(local_id),
+    foreign key(tempo_id) references tempo_dimensao(tempo_id),
+    foreign key(data_id) references data_dimensao(data_id),
+    primary key(numero));
