8b654964ca28b8590745f33961589f4ad0edf6c8
zemota1 <zemariamota@hotmail.com>
2016-12-13 20:11:02 +0000

    ESTA MAL

--- 4entrega/cube.sql
+++ 4entrega/cube.sql
@@ -6,9 +6,9 @@ group by data_id, local_id with rollup
 union
 select data_id, local_id, avg(montante_total) 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by data_id with rollup
-union
+group by local_id, data_id with rollup
+/*union
 select data_id, local_id, avg(montante_total) 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by  local_id with rollup 
-/*GROUP BY MAL*/
+group by   with rollup 
+/*TA MAL*/
