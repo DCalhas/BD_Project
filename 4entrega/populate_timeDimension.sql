5af349626d9d0cc231632744ab5605bd881e41f0
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2016-12-14 09:12:20 +0000

    Populate Date Dim quase feito, falta time

--- 4entrega/populate_timeDimension.sql
+++ 4entrega/populate_timeDimension.sql
@@ -1,11 +1,5 @@
 delimiter //
 
 drop procedure if exists populate_time;
-create procedure populate_time()
+create procedure load_time_dim()
 begin
-	declare full_date datetime;
-	set data_fim '2016-10-12 00:00:00';
-	set data_inicio '2016-10-11 00:00:00';
-	while data_inicio < data_fim
-	do 
-		insert into data_dimensao
