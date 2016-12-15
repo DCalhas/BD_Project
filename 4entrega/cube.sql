249a8113a959af749d07f6084606849cb7ceda11
DCalhas <davidcalhas@hotmail.com>
2016-12-15 12:14:28 +0000

    cube done

--- 4entrega/cube.sql
+++ 4entrega/cube.sql
@@ -1,17 +1,17 @@
 /*Cube*/
 
-select dia, mes, posto, espaco, avg(montante_total) 
+select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_total) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by dia, mes, posto, espaco with rollup 
+group by dia_do_ano, mes_do_ano, posto_codigo, espaco_codigo with rollup 
 union
-select dia, mes, posto, espaco, avg(montante_total) 
+select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_total) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by dia, mes, posto, espaco with rollup
+group by  mes_do_ano, posto_codigo,espaco_codigo, dia_do_ano with rollup
 union
-select data_id, local_id, avg(montante_total) 
+select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_total) as media
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by   with rollup 
+group by  posto_codigo, dia_do_ano, espaco_codigo, mes_do_ano with rollup 
 union
-select data_id, local_id, avg(montante_total) 
+select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_total) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by   with rollup 
+group by  espaco_codigo, dia_do_ano, mes_do_ano, posto_codigo with rollup;
