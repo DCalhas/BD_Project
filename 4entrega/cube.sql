514ea962bbebbafbed71d1f671ddf65af0c52522
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2016-12-16 18:07:38 +0000

    Add files via upload

--- 4entrega/cube.sql
+++ 4entrega/cube.sql
@@ -3,16 +3,40 @@
 select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
 group by dia_do_ano, mes_do_ano, posto_codigo, espaco_codigo with rollup 
+
 union
+
 select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by  mes_do_ano, posto_codigo, espaco_codigo, dia_do_ano with rollup
+group by mes_do_ano, posto_codigo, espaco_codigo, dia_do_ano with rollup
+
 union
+
 select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by  posto_codigo, espaco_codigo, dia_do_ano, mes_do_ano with rollup 
+group by posto_codigo, espaco_codigo, dia_do_ano, mes_do_ano with rollup 
+
 union
+
 select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by  espaco_codigo, dia_do_ano, mes_do_ano, posto_codigo with rollup;
-/*done*/
+group by espaco_codigo, dia_do_ano, mes_do_ano, posto_codigo with rollup;
+
+union
+
+select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media
+from reserva_factos natural join data_dimensao natural join local_dimensao
+group by mes_do_ano, espaco_codigo, posto_codigo, dia_do_ano with rollup;
+
+union
+
+select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media 
+from reserva_factos natural join data_dimensao natural join local_dimensao
+group by dia_do_ano, posto_codigo, mes_do_ano, espaco_codigo with rollup;
+
+/*ABCD
+BCDA
+CDAB
+DABC
+BDCA
+ACBD*/
