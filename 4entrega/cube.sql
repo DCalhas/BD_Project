43d7aaf80393704a88a5e9cbada3501012c39b65
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2016-12-19 11:36:58 +0000

    Cube corrigido, projeto done

--- 4entrega/cube.sql
+++ 4entrega/cube.sql
@@ -20,13 +20,13 @@ union
 
 select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media 
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by espaco_codigo, dia_do_ano, mes_do_ano, posto_codigo with rollup;
+group by espaco_codigo, dia_do_ano, mes_do_ano, posto_codigo with rollup
 
 union
 
 select dia_do_ano as dia, mes_do_ano as mes, posto_codigo as posto, espaco_codigo as espaco, avg(montante_pago) as media
 from reserva_factos natural join data_dimensao natural join local_dimensao
-group by mes_do_ano, espaco_codigo, posto_codigo, dia_do_ano with rollup;
+group by mes_do_ano, espaco_codigo, posto_codigo, dia_do_ano with rollup
 
 union
 
